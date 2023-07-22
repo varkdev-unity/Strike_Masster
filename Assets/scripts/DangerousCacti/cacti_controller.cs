9cc6e151b83b9c9a416e4d9a1647aa240ebb0a0a
aboba485 <112005553+aboba485@users.noreply.github.com>
2023-07-22 22:51:02 +0300

    arseny_5.0

---
+++ Assets/scripts/DangerousCacti/cacti_controller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class cacti_controller : MonoBehaviour
+{
+    private Animator cacti_animator;
+    private GameObject playerObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cacti_animator = GetComponent<Animator>();
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == playerObject)
+        {
+            //nothing
+        }
+    }
+    void start_shooting()
+    {
+
+    }
+
+
+}
